4e5fe3cadd79a798b832b1a2efd99f534b55208a
dirtyak <supmarvin@gmail.com>
2017-12-11 20:09:03 +0100

    Update xios_deploy.sh

--- xios_deploy.sh
+++ xios_deploy.sh
@@ -1,53 +1,84 @@
 #!/bin/bash
 #
+# XIOS_deploy is a script designed to install several XIOSd a way more simple
 #
-
-# Verify swapfile
-grep swap /etc/fstab
-if [ $? == 0 ]
-then
-        echo "swap exists, skipping..."
+# Usage : ./xios_deploy [n] 
+# Example : './xios_deploy 3' will deploy 3 XIOSd folder with files as .XIOS1 .XIOS2 and .XIOS3
+#
+#
+function usage {
+if [ -z $1 ]; then
+        echo "Usage: ./xios_deploy.sh [howmany]"
+        exit 0
 else
-        # Create 2GB swap file
-        fallocate -l 2G /swapfile
-        # Confirm creation of 2GB file
-        ls -ls /swapfile
-        # Set Permissions on swap file
-        chmod 600 /swapfile
-        # Enable swap file
-        mkswap 600 /swapfile
-        swapon /swapfile
-        # Make Changes Permanent
-        echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-        # Confirm Changes
-        free -h
+        echo "This will install XIOSd in each differents folder"
 fi
+}
 
-# Install Dependencies
+function verifyswap {
+        grep swap /etc/fstab
+        if [ $? == 0 ]
+        then
+                echo "swap exists, skipping..."
+        else
+                # Create 2GB swap file
+                fallocate -l 2G /swapfile
+                # Confirm creation of 2GB file
+                ls -ls /swapfile
+                # Set Permissions on swap file
+                chmod 600 /swapfile
+                # Enable swap file
+                mkswap 600 /swapfile
+                swapon /swapfile
+                # Make Changes Permanent
+                echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+                # Confirm Changes
+                free -h
+        fi
+}
+
+function satisfydependencies {
 apt-get -y update && apt-get -y install build-essential libssl-dev libdb++-dev libboost-all-dev libcrypto++-dev libqrencode-dev libminiupnpc-dev libgmp-dev libgmp3-dev$
+}
 
-# Clone Repo
-cd
-git clone https://github.com/davembg/xios
+function installXIOSd {
+# Verify if XIOSd is installed, if not install it
+        XIOSd_path="$HOME/xios/src/XIOSd"
+        if [ -f $XIOSd_path ]; then
+                echo "XIOSd is already installed."
+        else
+                echo "Let's Make XIOSd."
+                # Clone Repo
+                cd
+                git clone https://github.com/davembg/xios
+                # Create executable
+                cd $HOME/xios/src
+                make -f makefile.unix
+        fi
+}
 
-# Create executable
-cd xios/src
-make -f makefile.unix
-
-# Create XIOS configuration file
-mkdir $home/.XIOS1
-
-# Populate missing information in XIOS.conf file
-echo "rpcuser=<ANY user>" > $home/.XIOS1/.XIOS.conf
-echo "rpcpassword=<ANY password>" >> $home/.XIOS1/.XIOS.conf
-echo "rpcallowip=127.0.0.1" >> $home/.XIOS1/.XIOS.conf
-echo "rpcport=9100" >> $home/.XIOS1/.XIOS.conf
-echo "listen=1" >> $home/.XIOS1/.XIOS.conf
-echo "server=1" >> $home/.XIOS1/.XIOS.conf
-echo "daemon=1" >> $home/.XIOS1/.XIOS.conf
-echo "staking=0" >> $home/.XIOS1/.XIOS.conf
-echo "longtimestamps=1" >> $home/.XIOS1/.XIOS.conf
-echo "port=9000"  >> $home/.XIOS1/.XIOS.conf
-
-# Start XIOSd Application
-/root/xios/src/XIOSd -datadir=/root/.XIOS1 -config=/root/.XIOS1/XIOS.conf -daemon
+function makeconfigfiles {
+        i="1"
+        port=$[9000-1]
+        count=$1
+        staking=0
+        while [ $i -lt $[$count+1] ]
+                do
+                # Populate several XIOS.conf files
+                mkdir $HOME/.XIOS$i
+                touch $HOME/.XIOS$i/.XIOS.conf
+                echo "rpcuser=<ANY user>" > $HOME/.XIOS$i/.XIOS.conf
+                echo "rpcpassword=<ANY password>" >> $HOME/.XIOS$i/.XIOS.conf
+                echo "rpcallowip=127.0.0.1" >> $HOME/.XIOS$i/.XIOS.conf
+                echo "rpcport=9100" >> $HOME/.XIOS$i/.XIOS.conf
+                echo "listen=1" >> $HOME/.XIOS$i/.XIOS.conf
+                echo "server=1" >> $HOME/.XIOS$i/.XIOS.conf
+                echo "daemon=1" >> $HOME/.XIOS$i/.XIOS.conf
+                echo "staking=0" >> $HOME/.XIOS$i/.XIOS.conf
+                echo "longtimestamps=1" >> $HOME/.XIOS$i/.XIOS.conf
+                echo "port=$[$port+$i]"  >> $HOME/.XIOS$i/.XIOS.conf
+                # Start XIOSd deamon(s)
+                /root/xios/src/XIOSd -datadir=/root/.XIOS$i -config=/root/.XIOS$i/XIOS.conf -daemon
+        i=$[$i+1]
+        done
+}
